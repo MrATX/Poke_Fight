91034cbbd8d17abdcbb5f98066237c0330532895
John Whitaker <johnwhitakeraustin@gmail.com>
2020-08-14 17:36:03 -0700

    Poke Fight

---
+++ functions.py
@@ -0,0 +1,139 @@
+def ai_roster_sel(count_choice,poke):
+    import random
+    ros_count = int(count_choice)
+    idx = []
+    i = ros_count
+    while i > 0:
+        p2pick = random.randint(0,(len(poke)-1))
+        if p2pick not in idx:
+            idx.append(p2pick)
+            i = i - 1
+    global p2roster
+    p2roster = poke.iloc[[idx[i] for i in range(0,ros_count)],1:]
+    p2roster.reset_index(inplace=True)
+    p2roster = p2roster.iloc[:,1:]
+
+def player_roster_sel(plnum,players,count_choice,poke):
+        ros_count = int(count_choice)
+        poke_names = poke["Name"]
+        indexnums = poke["#"]
+        indexnums = str(indexnums)
+        idx = []
+        poke_choice = "hold"
+        plup = players[plnum-1][1]
+        print(poke.to_string(index = False))
+        print(f'\nWelcome {plup}! Please choose {count_choice} Pokemon from the list above.')
+        wip = "hold"
+        while wip == "hold":
+            poke_choice = input(f'\nUse the number in the left most column to make your selection --->>   ')
+            if poke_choice not in indexnums or poke_choice == "0" or poke_choice == "" or " " in poke_choice:
+                print(f"Make sure your input is a number (1 to {len(poke)}) --->>   ")
+            if poke_choice in indexnums and poke_choice != "0" and poke_choice != "" and " " not in poke_choice:
+                idx.append(int(poke_choice)-1)
+            if len(idx) == ros_count:
+                print("")
+                for i in idx:
+                    print(poke_names.iloc[i])
+                player_ready = input(f"\nAre these the Pokemon you want to take into battle, {plup}? (Y/N)   ")
+                ready_yes = ["Y","y"]
+                if player_ready in ready_yes and player_ready != "0" and player_ready != "" and " " not in player_ready:
+                    if plnum == 1:
+                        global p1roster
+                        p1roster = poke.iloc[[idx[i] for i in range(0,ros_count)],1:]
+                        p1roster.reset_index(inplace=True)
+                        p1roster = p1roster.iloc[:,1:]
+                    if plnum == 2:
+                        global p2roster
+                        p2roster = poke.iloc[[idx[i] for i in range(0,ros_count)],1:]
+                        p2roster.reset_index(inplace=True)
+                        p2roster = p2roster.iloc[:,1:]
+                    wip = "pass"
+                if player_ready not in ready_yes:
+                    idx = []
+
+def ai_poke_sel(roster):
+    import random
+    global aiidx
+    wip = "hold"
+    while wip == "hold":
+        aiidx = random.randint(0,len(p2roster)-1)
+        if p2roster.iloc[aiidx,4] > 0:
+            global p2active
+            p2active = p2roster.iloc[aiidx]
+            wip = "pass"
+            print(f"\nAI Opponent sends out {p2active[0]} - {p2active[4]} HP\n")
+
+def player_poke_sel(plnum,players,roster):
+    print(f"\n{players[plnum-1][1]}'s Pokemon")
+    print(roster.to_string(index=False))
+    global plactive
+    global plidx
+    plidx_range = []
+    for i in range(len(roster)):
+        plidx_range.append(str(i+1))
+    wip = "hold"
+    while wip == "hold":
+        plidx = input(f"\nWhich Pokemon would you like to send into battle, {players[plnum-1][1]}?\nSelect with index number (1 to {len(roster)})   ")
+        if plidx not in plidx_range or plidx == "" or " " in plidx or plidx == "0":
+            print(f"\nBe sure and use a proper index (1 to {len(roster)})   ")
+        if plidx in plidx_range and plidx != "" and " " not in plidx and plidx != "0":
+            plidx = int(plidx) - 1
+            if roster.iloc[plidx,3] == 0:
+                print(f"\n{roster.iloc[plidx,0]} is KO'd. Please select a different Pokemon.   ")
+            if roster.iloc[plidx,3] > 0:
+                plactive = roster.iloc[plidx]
+                wip = "pass"
+                print(f"\n{players[plnum-1][1]} sends out {plactive[0]} - {plactive[4]} HP")
+                if plnum == 1:
+                    global p1active
+                    p1active = plactive
+                if plnum == 2:
+                    global p2active
+                    p2active = plactive
+
+def attack(plnum,action,attacker,defender,atk_coef,def_coef,eva_coef):
+    import random
+    global defender_hp
+    if plnum == 1:
+        oppnum = 2
+    else:
+        oppnum = 1
+    attack_texts = ({"A":"attacked","a":"attacked","S":"used a special attack","s":"used a special attack"})
+    #Damage Calucation
+    if action == "A" or action == "a":
+        atk_dmg = int((atk_coef*attacker.loc["ATK"]*(1 - (def_coef*defender.loc["DEF"]))))
+    if action == "S" or action == "s":
+        atk_dmg = int((atk_coef*attacker.loc["SP ATK"]*(1-(def_coef*defender.loc["SP DEF"]))))
+    #Attack
+    if action == "S" or action == "s":
+        eva_coef = 0
+    if random.randint(1,100) > (eva_coef*defender.loc["SPEED"]):
+        defender_hp = defender.loc["HP"] - atk_dmg
+        if defender_hp < 0:
+            defender_hp = 0
+        if defender_hp == 0:
+            print(f"{attacker[0]} {attack_texts[action]} for {atk_dmg} DMG and KO'd {defender[0]}!")
+        if defender_hp > 0:
+            print(f'{attacker[0]} {attack_texts[action]} for {atk_dmg} DMG!')
+    else:
+        defender_hp = defender.loc["HP"]
+        print(f'{attacker[0]} MISSED!')
+    
+
+def HP_CHK(roster):
+    global hp
+    hp = 0
+    for i in range(0,len(roster)):
+        hp = hp + int(roster.iloc[i,3])
+
+
+def endgame(players,p1roster,p2roster,p1_ros_hp,p2_ros_hp):
+    print(f"\n{players[0][1]}'s Pokemon")
+    print(p1roster.to_string(index=False))
+    print(f"\n{players[1][1]}'s Pokemon")
+    print(p2roster.to_string(index=False))
+    print("")
+    if p1_ros_hp == "Fine":
+        print(f"{players[0][1]} wins!!!")
+    if p2_ros_hp == "Fine":
+        print(f"{players[1][1]} wins!!!")
---
+++ poke_fight.py
@@ -0,0 +1,177 @@
+#Poke Fight! ---------------------------------------------------------------
+
+#Import package, read file and clean data
+import pandas as pd
+import random
+import sys
+import functions as fn
+poke_file = "Pokemon/Pokemon_roster.csv"
+poke = pd.read_csv(poke_file)
+del poke["Legendary"]
+del poke["Generation"]
+del poke["Type 2"]
+poke.rename(columns={
+    "Type 1":"Type",
+    "Attack":"ATK",
+    "Defense":"DEF",
+    "Sp. Atk":"SP ATK",
+    "Sp. Def":"SP DEF",
+    "Speed":"SPEED",
+    "SPATK Count":"SP ATK Charges"
+},inplace=True)
+
+#Variables
+atk_coef = 0.35
+def_coef = 0.005
+eva_coef = 0.35
+pl_action = "hold"
+pl_sel_choices = ["A","a","S","s","P","p","R","r"]
+pl_sel_sp = ["S","s"]
+ai_action = "A"
+ai_sel_choices = ["A","S","P"]
+
+#Intro
+#------------------------------------------------------------
+#Mode Selection & Roster Count Selection
+intro = "hold"
+game_modes = ["1","2"]
+while intro not in game_modes:
+    intro = input("\nWelcome to Poke Fight!\nPlease select a game mode\n"
+    "[1] One Player\n[2] Two Player   ")
+count_choice = "hold"
+count_range = [str(i) for i in range(2,7)]
+while count_choice not in count_range:
+    count_choice = input("\nHow many Pokemon would you like to battle with? (2-6)   ")
+    
+#Name Selection & Roster Selection
+p1name = input("\nPlayer 1, please enter your name:   ")
+players = [("Player 1",p1name)]
+#One Player
+if intro == "1":
+    fn.player_roster_sel(1,players,count_choice,poke)
+    p1roster = fn.p1roster
+    p2name = "AI Opponent"
+    players.append(("Player 2",p2name))
+    fn.ai_roster_sel(count_choice,poke)
+    p2roster = fn.p2roster
+#Two Player
+if intro == "2":
+    p2name = input("\nPlayer 2, please enter your name:   ")
+    players.append(("Player 2",p2name))
+    fn.player_roster_sel(1,players,count_choice,poke)
+    p1roster = fn.p1roster
+    print("BREAK")
+    fn.player_roster_sel(2,players,count_choice,poke)
+    p2roster = fn.p2roster
+
+print(p2roster.iloc[1,4])
+
+#1st Pokemon_Selection
+#One Player
+if intro == "1":
+    print(f"\nAI Opponent's Pokemon: {p2roster.to_string(index=False)}")
+    fn.player_poke_sel(1,players,p1roster)
+    p1active = fn.plactive
+    p1idx = fn.plidx
+    fn.ai_poke_sel(p2roster)
+    p2active = fn.plactive
+    p2idx = fn.aiidx
+#Two Player
+if intro == "2":
+    fn.player_poke_sel(1,players,p1roster)
+    p1active = fn.plactive
+    p1idx = fn.plidx
+    fn.player_poke_sel(2,players,p2roster)
+    p2active = fn.plactive
+    p2idx = fn.plidx
+
+#Turn Loop
+#------------------------------------------------------------
+#Turn Order & Top While Loop
+p1_ros_hp = "Fine"
+p2_ros_hp = "Fine"
+while p1_ros_hp == "Fine" and p2_ros_hp == "Fine":
+    if p1active["SPEED"] > p2active["SPEED"]:
+        turn_ord = [1,2]
+    if p1active["SPEED"] < p2active["SPEED"]:
+        turn_ord = [2,1]
+    if p1active["SPEED"] == p2active["SPEED"]:
+        coin_toss = random.randint(0,100)
+        if coin_toss > 50:
+            turn_ord = [1,2]
+        if coin_toss < 50:
+            turn_ord = [2,1]
+    #Turn Actions & Inner For Loop
+    for i in turn_ord:
+        if i == 1:
+            plroster = p1roster
+            plactive = p1active
+            plidx = p1idx
+            oproster = p2roster
+            opactive = p2active
+            opidx = p2idx
+            j = 2
+        if i == 2:
+            plroster = p2roster
+            plactive = p2active
+            plidx = p2idx
+            oproster = p1roster
+            opactive = p1active
+            opidx = p1idx
+            j = 1
+        print(f"\n{players[i-1][1]}'s turn!\n{plactive[0]}")
+        if players[1][1] == "AI Opponent" and i == 2:
+            pl_action = ai_action
+        while pl_action not in pl_sel_choices:
+            pl_action = input(f"Choose Action:\n[A] Attack\n[S] Special Attack\n[P] Swap Pokemon\n[R] Retreat\n")
+            if pl_action in pl_sel_sp and plactive[9] == 0:
+                pl_action = "hold"
+                print(f"{plactive[0]} is out of special attacks!")
+        if pl_action in pl_sel_sp:
+            plroster.iloc[plidx,9] = int(plroster.iloc[plidx,9]) - 1
+        if pl_action == "R" or pl_action == "r":
+            print(f"{players[i-1][1]} retreated! {players[j-1][1]} wins")
+            sys.exit()
+        if pl_action == "P" or pl_action == "p":
+            fn.player_poke_sel(i,players,plroster)
+            plactive = fn.plactive
+            plidx = fn.plidx
+        else:
+            fn.attack(i,pl_action,plactive,opactive,atk_coef,def_coef,eva_coef)
+            oproster.iloc[opidx,3] = fn.defender_hp
+            opactive = oproster.iloc[opidx,:]
+        if opactive.iloc[3] == 0:  
+            fn.HP_CHK(oproster)
+            if fn.hp == 0:
+                if i == 1:
+                    p2_ros_hp = "KO'd"
+                if i == 2:
+                    p1_ros_hp = "KO'd"
+                fn.endgame(players,p1roster,p2roster,p1_ros_hp,p2_ros_hp)
+                sys.exit()
+            if players[1][1] == "AI Opponent" and i == 1:
+                fn.ai_poke_sel(p2roster)
+            else:
+                fn.player_poke_sel(j,players,oproster)
+            opactive = fn.plactive
+            opidx = fn.plidx
+        if i == 1:
+            p1roster = plroster
+            p1active = plactive
+            p1idx = plidx
+            p2roster = oproster
+            p2active = opactive
+            p2idx = opidx
+        if i == 2:
+            p2roster = plroster
+            p2active = plactive
+            p2idx = plidx
+            p1roster = oproster
+            p1active = opactive
+            p1idx = opidx
+        pl_action = "hold"
+    print("\n------------------------------------")
+    print(f"{p1active[0]} - {p1active[3]} HP")
+    print(f"{p2active[0]} - {p2active[3]} HP")
+    print("------------------------------------")
+
